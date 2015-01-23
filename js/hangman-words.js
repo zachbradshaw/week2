b137e02c46050c131b5984c26d6faad5ceb0abe5
zachbradshaw <bradshaw.zachary@gmail.com>
2015-01-23 00:07:45 -0500

    commented a lot of js

--- js/hangman-words.js
+++ js/hangman-words.js
@@ -14,21 +14,16 @@ var hangmanWords = [
 
 // setting variables
 var userLetter;
+
+var prevLetters = userLetter;
+
 var validWords = hangmanWords.filter(wordLength);
+
 var gameWord = getRandomWord();
+
 var turnCount = 6;
 
-// function that filters out all words in the hangmanWords variable
-// that are shorter than three letters
-function wordLength(word) {
-  return word.length >= 3;
-}
-
-// function that uses Math.floor and Math.random to pull a random word
-// out of the hangmanWords array
-function getRandomWord() {
-  return validWords[Math.floor(Math.random() * validWords.length)];
-}
+var gameMessage = "You have " + turnCount + " turn(s) left before this dude gets hanged...no pressure.";
 
 // turns gameWord into an array so its letters can be indexed
 var displayWord = gameWord.split('');
@@ -42,13 +37,58 @@ var dashedWord = displayWord.map(function (ch){
 // inserted into the DOM
 var finalDisplay = dashedWord.join(' ');
 
+// function that filters out all words in the hangmanWords variable
+// that are shorter than three letters
+function wordLength(word) {
+  return word.length >= 3;
+}
+
+// function that uses Math.floor and Math.random to pull a random word
+// out of the hangmanWords array
+function getRandomWord() {
+  return validWords[Math.floor(Math.random() * validWords.length)];
+}
+
 // inserts finalDisplay into the DOM
 document.querySelector('.dash-display').textContent = finalDisplay;
-document.querySelector('.turn-counter').textContent = turnCount;
+document.querySelector('.turn-counter').textContent = gameMessage;
 
+// Assigns the value (or letter) of the user's guess to userLetter, which was
+// previously undefined.
 function setGuess(guessValue) {
   userLetter = guessValue;
 }
+
+// the big one:
+// checkGuess is the function that powers hangman.
+//
+// Setting the variable 'correct' to FALSE allows the turnCount variable to
+// decrement one number for every wrong guess, it is set to TRUE within the
+// first if statement.
+//
+// The for loop starts at the 0 index of gameWord, or the first letter.
+// Depending on the length of the word, it will count "up" until it reaches the
+// end of the word, or the final character in the index.
+//
+// The first if statement checks to see if userLetter is equal to each
+// letter in the index of displayWord, an array compiled from the random word
+// generated with every page reload. If the userLetter matches one or more of
+// the letters in gameWord, those letters are revealed in the DOM and replace
+// the underscore placeholders. Setting the local variable 'correct' to TRUE
+// makes sure that turnCount stays the same on a correct guess.
+//
+// The second if statement if for an incorrect guess, if the letter goes through
+// the first if statement without finding a match, the local variable 'correct'
+// remains false, and turnCount goes down by one accordingly.
+//
+// The global variable 'finalDisplay' is reconverted back into a string using
+// the .join method.
+//
+// The correctly guessed letters are inserted into the DOM if applicable with
+// the first document.querySelector and the turnCount is updated using the
+// second document.querySelector.
+//
+// And that's what an informative comment looks like...I think.
 
 function checkGuess() {
   var correct = false
@@ -61,13 +101,17 @@ function checkGuess() {
   }
     if (correct === false) {
       turnCount = --turnCount;
+      prevLetters.push(guessValue);
     }
 
   finalDisplay = dashedWord.join(' ');
   document.querySelector('.dash-display').textContent = finalDisplay;
-  document.querySelector('.turn-counter').textContent = turnCount;}
+  document.querySelector('.turn-counter').textContent = "You have " + turnCount + " turn(s) left before this dude gets hanged...no pressure.";
+  document.querySelector('.prev-letters').textContent = prevLetters;
 
-// document.querySelector('.turn-counter').textContent = turnCount;
+}
+
+
 
 console.log(gameWord);
 console.log(turnCount);
